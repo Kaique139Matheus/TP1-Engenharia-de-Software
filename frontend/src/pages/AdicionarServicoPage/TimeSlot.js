3431add0eb7c4e5c3da5de7ee629e6b653dc2a0c
Kaique M Sousa <kaique139matheus@gmail.com>
2024-11-03 07:53:24 -0300

    Modificação na pagina de Adicionar serviço, para que não seja necessário criar várias cópias dos nomes dos dias da semana

--- frontend/src/pages/AdicionarServicoPage/TimeSlot.js
+++ frontend/src/pages/AdicionarServicoPage/TimeSlot.js
@@ -4,14 +4,16 @@ import Day from "./Day";
 import './TimeSlot.css'
 
 export default function TimeSlot({timeSlot}){
+    const daysNames = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
 
     return (
         <div className="TimeSlot">
             <p className="TextoTimeSlot">{timeSlot.inicio} : {timeSlot.fim}</p>
                 <flex className="DaysList">
                     {
-                        timeSlot.dias.map((elemento) => {
-                            return(<Day dia={elemento}/>)
+                        timeSlot.diasSelecionados.map((elemento, index) => {
+                            var dia = [daysNames[index], elemento]
+                            return(<Day dia={dia}/>)
                         })
                     }
                 </flex>
