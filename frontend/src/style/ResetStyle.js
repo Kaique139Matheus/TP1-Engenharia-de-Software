8e44b52cab2b59b48b1c3b7c4ba02ad6b0c80ebd
LeandroLopo <leandro.loopo@gmail.com>
2024-11-04 09:52:20 -0300

    Adição de hover e correção de erro na importação de fontes

--- frontend/src/style/ResetStyle.js
+++ frontend/src/style/ResetStyle.js
@@ -1,55 +1,58 @@
 import { createGlobalStyle } from "styled-components";
-import backgroundImage from "./kellen-riggin-edspQmByrTk-unsplash.jpg"
+import backgroundImage from "./kellen-riggin-edspQmByrTk-unsplash.jpg";
 
 const ResetStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
+
     html, body, div, span, applet, object, iframe,
-	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-	a, abbr, acronym, address, big, cite, code,
-	del, dfn, em, img, ins, kbd, q, s, samp,
-	small, strike, strong, sub, sup, tt, var,
-	b, u, i, center,
-	dl, dt, dd, ol, ul, li,
-	fieldset, form, label, legend,
-	table, caption, tbody, tfoot, thead, tr, th, td,
-	article, aside, canvas, details, embed, 
-	figure, figcaption, footer, header, hgroup, 
-	menu, nav, output, ruby, section, summary,
-	time, mark, audio, video {
-		margin: 0;
-		padding: 0;
-		border: 0;
-		font-size: 100%;
-		font: inherit;
-		vertical-align: baseline;
-	}
-	article, aside, details, figcaption, figure, 
-	footer, header, hgroup, menu, nav, section {
-		display: block;
-	}
-	body {
-		line-height: 1;
-		background-image: url(${backgroundImage});
-		background-size: cover;
-		background-position: center;
-	}
-	ol, ul {
-		list-style: none;
-	}
-	blockquote, q {
-		quotes: none;
-	}
-	blockquote:before, blockquote:after,
-	q:before, q:after {
-		content: '';
-		content: none;
-	}
-	table {
-		border-collapse: collapse;
-		border-spacing: 0;
-	}
-	* {
-		box-sizing: border-box;
-	}
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+    }
+    article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+    body {
+        line-height: 1;
+        background-image: url(${backgroundImage});
+        background-size: cover;
+        background-position: center;
+        font-family: 'Lexend Deca', sans-serif;
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+    * {
+        box-sizing: border-box;
+    }
 `
 
 export default ResetStyle;
